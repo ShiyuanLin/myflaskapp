52caa1993ae0602af68ef024001f723e634df5e7
ShiyuanLin <theolin@yahoo.com>
2018-08-07 13:48:19 -0400

    basic running

--- app.py
+++ app.py
@@ -3,8 +3,11 @@ from flask import Flask, render_template, flash, redirect, url_for, session, req
 from flask_mysqldb import MySQL
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from wtforms.fields.html5 import DateField
+from datetime import date, datetime
 from passlib.hash import sha256_crypt
 from functools import wraps
+# from web_function import get_activity_dic
+import web_function
 
 app = Flask(__name__)
 
@@ -174,11 +177,11 @@ def dashboard():
     # Get articles
     result = cur.execute("SELECT * FROM activities")
 
-    # articles = cur.fetchall()
+    activities = cur.fetchall()
 
     if result > 0:
         # return render_template('dashboard.html', articles=articles)
-        return render_template('dashboard.html')
+        return render_template('dashboard.html', activities=activities)
     else:
         msg = 'No Articles Found'
         return render_template('dashboard.html', msg=msg)
@@ -194,7 +197,7 @@ class ActivityForm(Form):
     min_time_end = DateField('Min End Time', format='%Y-%m-%d')
     max_time_end = DateField('Max End Time', format='%Y-%m-%d')
     duration_start = DateField('Duration Start Time', format='%Y-%m-%d')
-    duration_end = DateField('Duration Start Time', format='%Y-%m-%d')
+    duration_end = DateField('Duration End Time', format='%Y-%m-%d')
 
 # Add Activity
 @app.route('/add_activity', methods=['GET', 'POST'])
@@ -203,18 +206,24 @@ def add_activity():
     form = ActivityForm(request.form)
     if request.method == 'POST' and form.validate():
         title = form.title.data
-        min_time_start = form.min_time_start.data.strftime('%Y-%m-%d')
-        max_time_start = form.max_time_start.data.strftime('%Y-%m-%d')
-        min_time_end = form.min_time_end.data.strftime('%Y-%m-%d')
-        max_time_end = form.max_time_end.data.strftime('%Y-%m-%d')
 
         # Create Cursor
         cur = mysql.connection.cursor()
         
         # Execute
-        cur.execute("SELECT id from users where username = %s", [session.username])
-        userid = cur.fetchone()
-        cur.execute("INSERT INTO activities(user_id, activity) VALUES(%d, %s)", (userid, title))
+        cur.execute("SELECT id, department from users where username = %s", [session['username']])
+        result = cur.fetchone()
+        userid = result['id']
+        department = result['department']
+        cur.execute("INSERT INTO activities(user_id, activity) VALUES(%s, %s)", (userid, title))
+        # Add to RDF
+        web_function.add_activity(title, department, 
+            form.min_time_start.data.year, form.min_time_start.data.month, form.min_time_start.data.day, 
+            form.max_time_start.data.year, form.max_time_start.data.month, form.max_time_start.data.day,
+            form.min_time_end.data.year, form.min_time_end.data.month, form.min_time_end.data.day,
+            form.max_time_end.data.year, form.max_time_end.data.month, form.max_time_end.data.day,
+            form.duration_start.data.year, form.duration_start.data.month, form.duration_start.data.day,
+            form.duration_end.data.year, form.duration_end.data.month, form.duration_end.data.day,)
 
         # Commit to DB
         mysql.connection.commit()
@@ -222,12 +231,113 @@ def add_activity():
         #Close connection
         cur.close()
 
-        # To do - Add activity to the rdf
 
         flash('Activity Created', 'success')
 
         return redirect(url_for('dashboard'))
     return render_template('add_activity.html', form=form)
+
+
+# Show All Activities
+@app.route('/show_activity')
+@is_logged_in
+def show_activity():
+    # test_content = {'department1': 'title1', 'department2': 'title2'}
+    test_content = web_function.get_activity_dic()
+    return render_template('show_activity.html', content=test_content)
+
+
+# Delete Activity
+@app.route('/delete_activity/<string:title>', methods=['GET', 'POST'])
+@is_logged_in
+def delete_activity(title):
+    name=title
+    # Create cursor
+    cur = mysql.connection.cursor()
+
+    # Execute
+    cur.execute("DELETE FROM activities WHERE activity = %s", [title])
+
+    # Delete from RDF
+    web_function.delete_activity(name)
+
+    # Commit to DB
+    mysql.connection.commit()
+
+    #Close connection
+    cur.close()
+
+    flash('Activity Deleted', 'success')
+
+    return redirect(url_for('dashboard'))
+
+
+# Edit Article
+@app.route('/edit_activity/<string:title>', methods=['GET', 'POST'])
+@is_logged_in
+def edit_activity(title):
+    # print(web_function.get_activity_detail('Sewage_Apr_Activity'))
+    [start_min_year, start_min_month, start_min_day, 
+     start_max_year, start_max_month, start_max_day,
+     end_min_year, end_min_month, end_min_day,
+     end_max_year, end_max_month, end_max_day,
+     dur_min_year, dur_min_month, dur_min_day,
+     dur_max_year, dur_max_month, dur_max_day] = web_function.get_activity_detail(title)
+    # Get form
+    form = ActivityForm(request.form)
+    form.title.data = title
+    form.min_time_start.data = date(int(start_min_year), int(start_min_month), int(start_min_day))
+    form.max_time_start.data = date(int(start_max_year), int(start_max_month), int(start_max_day))
+    form.min_time_end.data = date(int(end_min_year), int(end_min_month), int(end_min_day))
+    form.max_time_end.data = date(int(end_max_year), int(end_max_month), int(end_max_day))
+    form.duration_start.data = date(int(dur_min_year), int(dur_min_month), int(dur_min_day))
+    form.duration_end.data = date(int(dur_max_year), int(dur_max_month), int(dur_max_day))
+
+
+
+    if request.method == 'POST' and form.validate():
+        # title = form.title.data
+        min_time_start = datetime.strptime(request.form['min_time_start'], '%Y-%m-%d')
+        max_time_start = datetime.strptime(request.form['max_time_start'], '%Y-%m-%d')
+        min_time_end = datetime.strptime(request.form['min_time_end'], '%Y-%m-%d')
+        max_time_end = datetime.strptime(request.form['max_time_end'], '%Y-%m-%d')
+        duration_start = datetime.strptime(request.form['duration_start'], '%Y-%m-%d')
+        duration_end = datetime.strptime(request.form['duration_end'], '%Y-%m-%d')
+
+        # print type(datetime.strptime(min_time_end, '%Y-%m-%d'))
+        # print(date(min_time_end))
+
+
+        # Create Cursor
+        cur = mysql.connection.cursor()
+        
+        # Execute
+        cur.execute("DELETE FROM activities where activity = %s", [title])
+        cur.execute("SELECT id, department from users where username = %s", [session['username']])
+        result = cur.fetchone()
+        userid = result['id']
+        department = result['department']
+        cur.execute("INSERT INTO activities(user_id, activity) VALUES(%s, %s)", (userid, request.form['title']))
+        # Add to RDF
+        web_function.edit_activity(title, request.form['title'], department, 
+            min_time_start.year, min_time_start.month, min_time_start.day, 
+            max_time_start.year, max_time_start.month, max_time_start.day,
+            min_time_end.year, min_time_end.month, min_time_end.day,
+            max_time_end.year, max_time_end.month, max_time_end.day,
+            duration_start.year, duration_start.month, duration_start.day,
+            duration_end.year, duration_end.month, duration_end.day)
+
+        # Commit to DB
+        mysql.connection.commit()
+
+        #Close connection
+        cur.close()
+
+
+        flash('Activity Modified', 'success')
+
+        return redirect(url_for('dashboard'))
+    return render_template('edit_activity.html', form=form)
 
 
 
